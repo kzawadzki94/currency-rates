7662001e65cbdb7229119a9b5cad19d2f07505a9
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2017-11-29 15:43:07 +0100

    Added css styling and footer. Implemented currencies rates downloading from api using factory and binding values to the view.

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ app.config(function($routeProvider, $locationProvider) {
             redirectTo: "currencies"
         })
         .when("/currencies", {
-            templateUrl: "/views/currencies.html"
+            templateUrl: "/views/currencies.html",
+            controller: "currencyController"
         })
         .when("/crypto", {
             templateUrl: "/views/crypto.html"
@@ -17,4 +18,38 @@ app.config(function($routeProvider, $locationProvider) {
         .otherwise({
             redirectTo: "/currencies"
         });
+});
+
+
+app.controller("currencyController", function($scope, dataFactory) {
+    dataFactory.getCurrencies("USD")
+        .then(function(response) {
+            $scope.currencies = getFormattedCurrency(response.data.rates)
+        }, function(error) {
+            console.log(error.message);
+        });
+
+    var getFormattedCurrency = function(data) {
+        var formatted = [];
+        var names = Object.keys(data);
+        var rates = Object.values(data);
+
+        for (var i = 0; i < names.length; i++) {
+            formatted.push({ name: names[i], rate: rates[i] });
+        }
+
+        return formatted;
+    };
+});
+
+app.factory("dataFactory", function($http) {
+    var dataFactory = {};
+    var currencyApi = "https://api.fixer.io/latest?base=";
+
+    dataFactory.getCurrencies = function(baseCurrency) {
+        var request = currencyApi + baseCurrency;
+        return $http.get(request);
+    };
+
+    return dataFactory;
 });
