49d3036b7eb8e85a40cc9700045e0e3d8855fdae
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2017-11-29 18:31:48 +0100

    Added base currency select option

--- js/app.js
+++ js/app.js
@@ -1,55 +1,63 @@
 var app = angular.module("app", ["ngRoute"]);
 
 // Routing
-app.config(function($routeProvider, $locationProvider) {
-    $locationProvider.hashPrefix("");
+app.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix("");
 
-    $routeProvider
-        .when("/", {
-            redirectTo: "currencies"
-        })
-        .when("/currencies", {
-            templateUrl: "/views/currencies.html",
-            controller: "currencyController"
-        })
-        .when("/crypto", {
-            templateUrl: "/views/crypto.html"
-        })
-        .otherwise({
-            redirectTo: "/currencies"
-        });
+  $routeProvider
+    .when("/", {
+      redirectTo: "currencies"
+    })
+    .when("/currencies", {
+      templateUrl: "/views/currencies.html",
+      controller: "currencyController"
+    })
+    .when("/crypto", {
+      templateUrl: "/views/crypto.html"
+    })
+    .otherwise({
+      redirectTo: "/currencies"
+    });
 });
 
+app.controller("currencyController", function ($scope, dataFactory) {
+  $scope.base = "USD";
 
-app.controller("currencyController", function($scope, dataFactory) {
-    dataFactory.getCurrencies("USD")
-        .then(function(response) {
-            $scope.currencies = getFormattedCurrency(response.data.rates)
-        }, function(error) {
-            console.log(error.message);
-        });
+  $scope.getData = function () {
+    dataFactory.getCurrencies($scope.base).then(
+      function (response) {
+        $scope.currencies = getCurrencyArray(response.data.rates);
+      },
+      function (error) {
+        console.log(error.message);
+      }
+    );
+  };
 
-    var getFormattedCurrency = function(data) {
-        var formatted = [];
-        var names = Object.keys(data);
-        var rates = Object.values(data);
+  var getCurrencyArray = function (data) {
+    var formatted = [];
+    var names = Object.keys(data);
+    var rates = Object.values(data);
 
-        for (var i = 0; i < names.length; i++) {
-            formatted.push({ name: names[i], rate: rates[i] });
-        }
+    for (var i = 0; i < names.length; i++) {
+      formatted.push({
+        name: names[i],
+        rate: rates[i]
+      });
+    }
 
-        return formatted;
-    };
+    return formatted;
+  };
 });
 
-app.factory("dataFactory", function($http) {
-    var dataFactory = {};
-    var currencyApi = "https://api.fixer.io/latest?base=";
+app.factory("dataFactory", function ($http) {
+  var dataFactory = {};
+  var currencyApi = "https://api.fixer.io/latest?base=";
 
-    dataFactory.getCurrencies = function(baseCurrency) {
-        var request = currencyApi + baseCurrency;
-        return $http.get(request);
-    };
+  dataFactory.getCurrencies = function (baseCurrency) {
+    var request = currencyApi + baseCurrency;
+    return $http.get(request);
+  };
 
-    return dataFactory;
+  return dataFactory;
 });
