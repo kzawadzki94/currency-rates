e2d2da3ec57920ad094680bcc99bf52e8bf6048b
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-01-09 22:14:09 +0100

    Added loading currency fullnames from file. Refactoring.

--- js/app.js
+++ js/app.js
@@ -21,48 +21,21 @@ app.config(function($routeProvider, $locationProvider) {
 });
 
 app.controller("currencyController", function($scope, dataFactory) {
-  $scope.countries = {
-    PLN: "polski złoty",
-    EUR: "euro",
-    USD: "dolar amerykański",
-    GBP: "funt brytyjski",
-    CHF: "frank szwajcarski",
-    AUD: "dolar australijski",
-    BGN: "lew bułgarski",
-    BRL: "real brazylijski",
-    CAD: "dolar kanadyjski",
-    CNY: "chiński Juan (Renminbi)",
-    CZK: "korona czeska",
-    DKK: "korona duńska",
-    HKD: "dolar hongkoński",
-    HRK: "kuna chorwacka",
-    HUF: "forint węgierski",
-    IDR: "rupia indonezyjska",
-    ILS: "nowy szekel izraelski",
-    INR: "rupia indyjska",
-    JPY: "jen japoński",
-    KRW: "won południowokoreański",
-    MXN: "peso meksykańskie",
-    MYR: "ringgit malezyjski",
-    NOK: "korona norweska",
-    NZD: "dolar nowozelandzki",
-    PHP: "peso filipińskie",
-    RON: "nowa leja rumuńska",
-    RUB: "rubel rosyjski",
-    SEK: "korona szwedzka",
-    SGD: "dolar singapurski",
-    THB: "baht tajski",
-    TRY: "nowa lira turecka",
-    ZAR: "rand południowoafrykański"
-  };
-
-  $scope.names = Object.keys($scope.countries);
-  $scope.base = $scope.names[0];
-
+  $scope.base = "PLN";
   $scope.getData = function() {
-    dataFactory.getCurrencies($scope.base).then(
+    dataFactory.getJSONFromFile("/js/currency-fullnames.json").then(
       function(response) {
-        $scope.currencies = getCurrencyArray(response.data.rates);
+        $scope.fullnames = response.data.fullnames;
+        $scope.names = Object.keys($scope.fullnames);
+
+        dataFactory.getRates($scope.base).then(
+          function(response) {
+            $scope.rates = getFormattedRates(response.data.rates);
+          },
+          function(error) {
+            console.log(error.message);
+          }
+        );
       },
       function(error) {
         console.log(error.message);
@@ -71,21 +44,6 @@ app.controller("currencyController", function($scope, dataFactory) {
 
     $scope.sortType = localStorage.getItem("sortType");
     $scope.sortReverse = JSON.parse(localStorage.getItem("sortReverse"));
-  };
-
-  var getCurrencyArray = function(data) {
-    var formatted = [];
-    var names = Object.keys(data);
-    var rates = Object.values(data);
-
-    for (var i = 0; i < names.length; i++) {
-      formatted.push({
-        name: names[i],
-        fullname: $scope.countries[names[i]],
-        rate: rates[i]
-      });
-    }
-    return formatted;
   };
 
   $scope.setSorting = function(type) {
@@ -98,13 +56,32 @@ app.controller("currencyController", function($scope, dataFactory) {
       localStorage.sortReverse = false;
     }
   };
+
+  var getFormattedRates = function(data) {
+    var formatted = [];
+    var names = Object.keys(data);
+    var rates = Object.values(data);
+
+    for (var i = 0; i < names.length; i++) {
+      formatted.push({
+        name: names[i],
+        fullname: $scope.fullnames[names[i]],
+        rate: rates[i]
+      });
+    }
+    return formatted;
+  };
 });
 
 app.factory("dataFactory", function($http) {
   var dataFactory = {};
   var currencyApi = "https://api.fixer.io/latest?base=";
 
-  dataFactory.getCurrencies = function(baseCurrency) {
+  dataFactory.getJSONFromFile = function(filePath) {
+    return $http.get(filePath);
+  };
+
+  dataFactory.getRates = function(baseCurrency) {
     var request = currencyApi + baseCurrency;
     return $http.get(request);
   };
